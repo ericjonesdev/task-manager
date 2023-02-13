d58e44f45d3dddde69616b1a79e34261a208e625
ericjonesdev <ericjones2004@yahoo.com>
2023-02-13 21:00:46 +0000

    Add routes to categories and add_category links

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,13 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template("add_category.html")
