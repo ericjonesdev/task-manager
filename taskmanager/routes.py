20b3d1832a941bb9a59bd793bb18d1823c046102
ericjonesdev <ericjones2004@yahoo.com>
2023-02-14 20:20:13 +0000

    Copy repo add_category to main file for troubleshooting

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -15,4 +15,9 @@ def categories():
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
